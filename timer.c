07737b1766ed42077d7c16adf2534cf44962f776
Olle J <user@LAPTOP-CRHEC88J.localdomain>
2021-11-02 09:59:42 +0100

    Cleaned upp bit-masks

--- timer.c
+++ timer.c
@@ -10,8 +10,8 @@ void timer_init() {
 	DDRB = 0xff;
 
 	//Set CTC(Clear timer on compare match) in (Timer/Counter Control Register A)
-	TCCR0A = (1<<WGM01); //Set WGM01 to 1
-	//TCCR0A &= ~(1<<WGM00); //Set WGM00 to 0
+	TCCR0A |= (1<<WGM01); //Set WGM01 to 1
+	TCCR0A &= ~(1<<WGM00); //Set WGM00 to 0
 
 	//Calculation for getting a time period of 10ms: 
 	//(16000000/((155+1)*1024)) = 100.160... hz
@@ -23,10 +23,10 @@ void timer_init() {
 	sei(); //Set external I-bit to enable interrupts
 
 	//Set prescale to 1024 in register(Timer/Counter Control Register B)
-	//TCCR0B &= ~(1<<WGM02); //Set WGM02 to 0
-	TCCR0B = (1<<CS02) | (1<< CS00); //Set CS02:1 and CS00:1
-	//TCCR0B &= ~(1<<CS01); //Set CS01 to 0
-	//TCCR0B &= (1<<CS00);
+	TCCR0B &= ~(1<<WGM02); //Set WGM02 to 0
+	TCCR0B |= (1<<CS02); //Set CS02 to 1
+	TCCR0B &= ~(1<<CS01); //Set CS01 to 0
+	TCCR0B |= (1<<CS00); //and CS00 to 1
 
 	//TCNT0 = 0; //Start timer
 }
@@ -34,7 +34,7 @@ void timer_init() {
 ISR(TIMER0_COMPA_vect) {
 	divider++;
 	
-	if(divider == 100) {
+	if(divider == 10) {
 		blueLight();
 	} else {
 		led_off();
