8c3fb9b78ca3d679806777afd6d4b6f3cee0427e
Olle J <user@LAPTOP-CRHEC88J.localdomain>
2021-11-02 11:59:53 +0100

    attempt at using mode 3(fast PWM)

--- timer.c
+++ timer.c
@@ -1,40 +1,36 @@
 #include <avr/io.h>
-#include <avr/interrupt.h>
 
 #include "timer.h"
 #include "led.h"
 
-volatile uint8_t divider = 0; 
+volatile uint8_t counter = 0; 
 
 void timer_init() {
-	DDRB = 0xff;
-
-	//Set CTC(Clear timer on compare match) in (Timer/Counter Control Register A)
+	//Set Fast PWM in (Timer/Counter Control Register A)	
 	TCCR0A |= (1<<WGM01); //Set WGM01 to 1
-	TCCR0A &= ~(1<<WGM00); //Set WGM00 to 0
+	TCCR0A &= (1<<WGM00); //Set WGM00 to 1
+	//Clear OC0A on Compare match, set OC0A at bottom(non-inverting mode)
+	//TCCR0A &= ~(1<<COM0A0); //Set COM0A0 to 0
+	TCCR0A &= (1<<COM0A1); //Set COM0A1 to 1
+	
+	//(16000000/((254+1)*64)) = 980.3921568627451 Hz
+	//OCR0A = 254; //The TOP-value for compare matching with TCNT0
 
-	//Calculation for getting a time period of 10ms: 
-	//(16000000/((155+1)*1024)) = 100.160... hz
-	OCR0A = 156; //The TOP-value for compare matching with TCNT0
-
-	//Set Timer Interrupt Mask Register
-	TIMSK0 = (1<<OCIE0A);  //Set Output Compare Interrupt Enable Match A to 1
-
-	sei(); //Set external I-bit to enable interrupts
-
-	//Set prescale to 1024 in register(Timer/Counter Control Register B)
+	//Set prescale to 64 in register(Timer/Counter Control Register B)
 	TCCR0B &= ~(1<<WGM02); //Set WGM02 to 0
-	TCCR0B |= (1<<CS02); //Set CS02 to 1
-	TCCR0B &= ~(1<<CS01); //Set CS01 to 0
-	TCCR0B |= (1<<CS00); //and CS00 to 1
+	TCCR0B &= ~(1<<CS02); //Set CS02 to 0
+	TCCR0B |= (1<<CS01); //Set CS01 to 1
+	TCCR0B &= (1<<CS00); //and CS00 to 1
 
 	TCNT0 = 0; //Start timer
 }
 
-ISR(TIMER0_COMPA_vect) {
-	divider++;
-	
-	if(divider == 10) {
+void checkCounter(void) {
+	if(TCNT0 >= 255) { //If statement to check if timer has passed top value
+		counter++; //Increment global variable
+		counter %= 100; //Modulus 100 for more accurate count
+	}
+	if (counter == 10) { //If global variable has reached a count of 10, turn on blue light
 		blueLight();
 	} else {
 		led_off();
--- timer.h
+++ timer.h
@@ -2,6 +2,6 @@
 #define _TIMER_H_
 
 void timer_init(void);
-ISR(TIMER0_COMPA_vect);
+void checkCounter(void);
 
 #endif // _TIMER_H_
